f49417d25141e3949c85e9aa63ef82f6b8df4a32
tush87 <tusharj8788@gmail.com>
2023-05-17 12:54:17 +0530

    add task done or not

--- controller/task_controller.js
+++ controller/task_controller.js
@@ -24,3 +24,32 @@ module.exports.delete=async function(req,res){
 
     }
 }
+
+//task done or not
+module.exports.taskDoneOrNot=async function(req,res){
+    try{
+        // console.log(req.query);
+
+        let list = await ListDB.findById(req.query.listId);
+
+        //check task found or user match or not 
+        if(!list || list.user != req.user.id){
+            console.log("user not match or task not found");
+            return res.redirect('/user/signout');
+        }
+        
+        for (let t of list.tasks){
+            if(t._id==req.query.taskId){
+                t.done=req.query.done;
+                break;
+            }
+        }
+
+        await list.save();
+        return res.redirect('back');
+    }
+    catch(err){
+        console.log(err);
+        return res.redirect('back');
+    }
+}
--- routes/task.js
+++ routes/task.js
@@ -3,6 +3,10 @@ const taskController = require('../controller/task_controller');
 const passport = require('passport');
 const router = express.Router();
 
+//detele task from list
 router.get('/delete',passport.checkAuthentication,taskController.delete);
+
+//task done or not
+router.get('/doneornot',passport.checkAuthentication,taskController.taskDoneOrNot);
 
 module.exports = router;
